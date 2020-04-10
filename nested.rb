43bc2878ff1cb15309e5c3245d22bbb4f7578a6f
Douglas Manuel Barco Martin <douglas@dobuilding.co.uk>
2020-04-10 03:20:50 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -126,8 +126,8 @@ def adding_to_dennis
       },
       :dennis_ritchie => {
         :known_for => "Unix",
-        :languages => ["C"]
+        :languages => ["C","Assembly"]
       }
     }
-programmer_hash[:dennis_ritchie][:languages][1]="Assembly"
+# programmer_hash[:dennis_ritchie][:languages][1]="Assembly"
 end
